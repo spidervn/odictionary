44afbb5182953008d87c866d5dc3820d5ab33951
ducvd <truyenspidermanvn@gmail.com>
2019-02-28 15:43:37 +0700

    Define data structure

---
+++ src/odict/app/Interface/ASubMeaning.java
@@ -0,0 +1,12 @@
+package odict.app.Interface;
+
+import java.util.ArrayList;
+
+public class ASubMeaning {
+	String meaning;
+	ArrayList<String> examples;
+	
+	public ASubMeaning() {
+		examples = new ArrayList<String>();
+	}
+}
