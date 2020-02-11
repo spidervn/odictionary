5df1beae69625874e47e03852371908635d35d40
ducvd <vanduyduc@gmail.com>
2020-02-11 17:57:52 +0700

    get whole word & grammar

--- src/python-project/oneword.py
+++ src/python-project/oneword.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib2
-import enum
+from json import JSONEncoder
+import json
 
 #
 # Oneword to json
@@ -13,52 +14,90 @@ WordType_Preposition = 4
 WordType_Phrases = 5
 WordType_Unknown = 6
 
-class MeaningItem:
-    self.meaning = ""
-    self.extra_note = ""
-    self.examples = []
-
-    def MeaningItem(self):
+class MeaningItem(JSONEncoder):
+    def __init__(self):
+        self.meaning = ""
+        self.extra_note = ""
+        self.examples = []
         return
 
-class MeaningNote:
-    def MeaningNote(self):
+class MeaningNote(JSONEncoder):
+
+    def __init__(self, name, age):
         self.meaning = MeaningItem()
         self.child_meanings = []    # Array of meaning items
         return
 
-class OneWord:
-    self.name = ""              # 
-    self.wordType = "Unknown"   # 
-    self.pronunciations=[]      # Array of pronunciations
+class OneWord(JSONEncoder):
+    def __init__(self):
+        self.name = ""              # 
+        self.wordType = "Unknown"   # 
+        self.pronunciations=[]      # Array of pronunciations
+        self.meaning_by_wordtype = []   # Array of (Array of Meaning notes)
+        self.origin = ""                # History
+        self.alternatives = []          # Array of OneWord (Alternative meanings/or wordtype)
 
-    self.meaning_by_wordtype = []   # Array of (Array of Meaning notes)
-    self.origin = ""                # History
-    self.alternatives = []          # Array of OneWord (Alternative meanings/or wordtype)
-    
-    def OneWord(self):
-        return
-
-    def OneWord(self):
+        self.encoding = "UTF-8"
         return
 
 class ScrapOneWord:
-
     def ScrapOneWord(self):
         return
 
-fr = urllib2.urlopen("https://en.oxforddictionaries.com/definition/odometry")
-html = fr.read()
-fr.close()
+def scrape_oneword(wordurl):
+    fr = urllib2.urlopen(wordurl)
+    html = fr.read()
+    fr.close()
 
-# page = requests.get("https://en.oxforddictionaries.com/definition/odometry")
-soup = BeautifulSoup(html) # page.content, "html.parser")
+    soup = BeautifulSoup(html)
 
-print soup.title
-print soup.p
+    divMain = soup.find("div", "entryWrapper")
 
-a = soup.find(class="hwg")
-for link in a:
-    print "One: "
-    print a
+    wns = divMain.find_all("span", "hw")   # Word names
+    aus = divMain.find_all("audio")        # Audio
+
+    wordtypes = divMain.find_all("span", "pos")
+    gramms = divMain.find_all("section", "gramb")
+    sembs = divMain.find_all("ul", "semb")
+
+    for wn in wns:
+        print(wn)
+    
+    for au in aus:
+        print(au)
+
+    for wt in wordtypes:
+        print(wt)
+    
+    print("There are grammars ", len(gramms))
+    print("There are sembs ", len(sembs))
+
+    for semb in sembs:
+        print(semb.text)
+
+    return
+
+# wordurl = "https://www.lexico.com/definition/do"
+# fr = urllib2.urlopen(wordurl)
+# html = fr.read()
+# fr.close()
+
+# # page = requests.get("https://en.oxforddictionaries.com/definition/odometry")
+# soup = BeautifulSoup(html) # page.content, "html.parser")
+
+# print soup.title
+# print soup.p
+
+# a = soup.find(class="hwg")
+# for link in a:
+#     print "One: "
+#     print a
+
+w = OneWord()
+w.name = "publicity"
+
+s = json.dumps(w.__dict__)
+print(s)
+
+scrape_oneword("https://www.lexico.com/definition/he")
 
